3858ae439bb1a0986f2736987d10dc5a8be65f38
c-auri <43008483+c-auri@users.noreply.github.com>
2023-06-30 19:48:57 +0200

    Introduce Candidates with values

--- src/AI.ts
+++ src/AI.ts
@@ -1,23 +1,30 @@
 import { Board } from "./ts/model/Board"
 
-type Coordinate = { x: number, y: number }
+type Candidate = { x: number, y: number, value: number }
 
 export function findTarget(board: Board) {
-    const candidates: Coordinate[] = []
+    const candidates: Candidate[] = []
 
     for (let x = 0; x < Board.Size; x++) {
         for (let y = 0; y < Board.Size; y++) {
             if (board.getState(x, y) === 'unknown') {
-                candidates.push({ x, y })
+                candidates.push({ x, y, value: 1 })
             }
         }
     }
 
-    return pickAtRandom(candidates)
+    return pickAtRandom(selectBest(candidates))
 }
 
-function pickAtRandom(coordinates: Coordinate[]) {
-    const maxIndex = coordinates.length - 1
+function selectBest(candidates: Candidate[]) {
+    const maxValue = Math.max(...candidates.map(c => c.value))
+    return candidates.filter(c => c.value === maxValue)
+}
+
+function pickAtRandom(candidates: Candidate[]) {
+    const maxIndex = candidates.length - 1
     const randomIndex = Math.round(Math.random() * (maxIndex))
-    return coordinates[randomIndex]
+    const result = candidates[randomIndex]
+
+    return { x: result.x, y: result.y }
 }
