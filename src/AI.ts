25d30aa3635d511df115cc020a3db807b3d9d25c
c-auri <43008483+c-auri@users.noreply.github.com>
2023-06-30 18:09:19 +0200

    Refactor findTarget

--- src/AI.ts
+++ src/AI.ts
@@ -1,13 +1,23 @@
 import { Board } from "./ts/model/Board"
 
+type Coordinate = { x: number, y: number }
+
 export function findTarget(board: Board) {
-    let x: number
-    let y: number
+    const candidates: Coordinate[] = []
 
-    do {
-        x = Math.round(Math.random() * 9)
-        y = Math.round(Math.random() * 9)
-    } while(board.getState(x, y) !== 'unknown')
+    for (let x = 0; x < Board.Size; x++) {
+        for (let y = 0; y < Board.Size; y++) {
+            if (board.getState(x, y) === 'unknown') {
+                candidates.push({ x, y })
+            }
+        }
+    }
 
-    return { x, y }
+    return pickAtRandom(candidates)
+}
+
+function pickAtRandom(coordinates: Coordinate[]) {
+    const maxIndex = coordinates.length - 1
+    const randomIndex = Math.round(Math.random() * (maxIndex))
+    return coordinates[randomIndex]
 }
