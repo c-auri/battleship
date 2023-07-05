52aa4f6066ed4358e1dfb25648581a6e7a51d1fb
c-auri <43008483+c-auri@users.noreply.github.com>
2023-07-06 00:13:23 +0200

    Add close button to modal

--- src/view/GameOver.ts
+++ src/view/GameOver.ts
@@ -1,9 +1,11 @@
 import { initialize } from "../controller/Game"
 
-const newGameButton = document.getElementById('new-game-button') as HTMLButtonElement
 const gameOverDialog = document.getElementById('game-over-dialog') as HTMLDialogElement
+const closeButton = document.getElementById('close-button') as HTMLButtonElement
 const resultSpan = document.getElementById('result') as HTMLSpanElement
+const newGameButton = document.getElementById('new-game-button') as HTMLButtonElement
 
+closeButton.addEventListener('click', () => gameOverDialog?.close())
 newGameButton.addEventListener('click', startNewGame)
 
 export function showGameOver(playerWon: boolean) {
