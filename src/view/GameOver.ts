c317fdddd4d4fffcd141028c48242992378ccbc3
c-auri <43008483+c-auri@users.noreply.github.com>
2023-07-06 00:00:18 +0200

    Fix page reload on modal close

--- src/view/GameOver.ts
+++ src/view/GameOver.ts
@@ -1,11 +1,17 @@
 import { initialize } from "../controller/Game"
 
+const newGameButton = document.getElementById('new-game-button') as HTMLButtonElement
 const gameOverDialog = document.getElementById('game-over-dialog') as HTMLDialogElement
 const resultSpan = document.getElementById('result') as HTMLSpanElement
 
-gameOverDialog.addEventListener('close', initialize)
+newGameButton.addEventListener('click', startNewGame)
 
 export function showGameOver(playerWon: boolean) {
     gameOverDialog?.showModal()
     resultSpan.textContent = playerWon ? 'You won!' : 'You lost!'
+}
+
+function startNewGame() {
+    gameOverDialog?.close()
+    initialize()
 }
